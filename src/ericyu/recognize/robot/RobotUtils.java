bcb8c126c727982fa65c8a663ee4551442930f90
eric yu <yulin.jay@gmail.com>
2015-11-24 23:13:12 +0800

    add robot mouse actions

--- src/ericyu/recognize/robot/RobotUtils.java
+++ src/ericyu/recognize/robot/RobotUtils.java
@@ -8,6 +8,7 @@ package ericyu.recognize.robot;
  +===========================================================================*/
 
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,31 +34,91 @@ public class RobotUtils
 
     }
 
-    public static void doRobotThing(ArrayList<Integer> numbers)
+    public static void enterVerificationCode(ArrayList<Integer> numbers)
     {
         if (numbers.size() != 4)
         {
-            System.out.println("robot can not work");
+            System.out.println("there is no 4robot can not work");
         }
+
+        for (int num : numbers)
+        {
+            pressKey(keyMap.get(num));
+            System.out.println("robot pressed number " + num);
+        }
+
+    }
+
+    public static void pressKey(int key)
+    {
         try
         {
-            Robot robot = new Robot();
-            for (int num : numbers)
-            {
-                keyPress(robot, keyMap.get(num));
-                System.out.println("robot pressed number " + num);
-            }
-        } catch (Exception e)
+            Robot r = new Robot();
+            r.keyPress(key);
+            r.keyRelease(key);
+            r.delay(100);
+        } catch (AWTException e)
         {
             e.printStackTrace();
         }
     }
 
-    public static void keyPress(Robot r, int key)
+    public static void clickAt(int x, int y)
     {
-        r.keyPress(key);
-        r.keyRelease(key);
-        r.delay(100);
+        try
+        {
+            Robot r = new Robot();
+            r.mouseMove(x,y);
+            r.mousePress(InputEvent.BUTTON1_MASK);
+            r.mouseRelease(InputEvent.BUTTON1_MASK);
+        } catch (AWTException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static Point findPosition()
+    {
+        try
+        {
+            Robot robot = new Robot();
+            Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+            for(int i = 0; i <dimension.height; ++i)
+            {
+                for(int j = 0 ; j < dimension.width-1; ++j)
+                {
+                    if(robot.getPixelColor(j,i) == new Color(25,31,43)/* && robot.getPixelColor(j+1,i) == new Color(25,31,45) && robot.getPixelColor(j,i+1) == new Color(24,33,48)*/)
+                    {
+                        return new Point(j,i);
+                    }
+                }
+            }
+        } catch (AWTException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+//        try
+//        {
+//            Robot r = new Robot();
+//            while(true)
+//            {
+//                Point point = MouseInfo.getPointerInfo().getLocation();
+//                Color color = r.getPixelColor(point.x, point.y);
+//                System.out.println("x:" + point.x + " y:" + point.y + " color: " + color);
+//                r.delay(1000);
+//            }
+//        } catch (AWTException e)
+//        {
+//            e.printStackTrace();
+//        }
+        Point point = findPosition();
+        System.out.println(point.x + "  " + point.y);
+        clickAt(point.x,point.y);
     }
 
 }
