59648e5b359f529a99d51f2c29038ae48aac5c43
Eric YU <lin.yu@oracle.com>
2015-11-26 14:12:30 +0800

    ocr first commit

--- src/ericyu/chepai/robot/Test.java
+++ src/ericyu/chepai/robot/Test.java
@@ -7,13 +7,29 @@ package ericyu.chepai.robot;
  |           Created by lliyu on 11/25/2015  (yulin.jay@gmail.com)           |
  +===========================================================================*/
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.opencv.core.Core;
+
+import java.io.File;
 
 public class Test
 {
     public static void main(String[] args)
     {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        File imageFile = new File("screenCapture.bmp");
+        Tesseract instance = new Tesseract();
+
+        try
+        {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        }
+        catch (TesseractException e)
+        {
+            e.printStackTrace();
+        }
 
 
     }
