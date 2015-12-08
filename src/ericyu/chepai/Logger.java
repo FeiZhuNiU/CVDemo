ecc8298299595920400990c87d625d25521e4c49
eric yu <yulin.jay@gmail.com>
2015-12-08 23:57:34 +0800

    send to data server first commit

--- src/ericyu/chepai/Logger.java
+++ src/ericyu/chepai/Logger.java
@@ -7,11 +7,17 @@ package ericyu.chepai;
  |           Created by lliyu on 11/30/2015  (yulin.jay@gmail.com)           |
  +===========================================================================*/
 
+import com.aliyun.oss.OSSClient;
 import ericyu.chepai.flash.FlashStatusDetector;
 
 
 public class Logger
 {
+
+    private static String accessKeyId = "Ndg6XzPda5JTNFpa";
+    private static String accessKeySecret = "qHZ3Q5oJCCMv9Xrfwr2b6KVjwre0Zc";
+    private static String endPoint  = "feizhuniu.oss-cn-shanghai.aliyuncs.com";
+
     public enum Level
     {
         INFO,
@@ -22,5 +28,12 @@ public class Logger
     public static void log(Level level, FlashStatusDetector.Status status, String message)
     {
         System.out.println(level + ": ["+ DateUtil.getCurrentTime() +"] [FlashStatus:" + status + "] : " + message);
+//        sendToDataServer(level,status,message);
+    }
+
+    private static void sendToDataServer(Level level, FlashStatusDetector.Status status, String message)
+    {
+        OSSClient client = new OSSClient(endPoint,accessKeyId,accessKeySecret);
+
     }
 }
